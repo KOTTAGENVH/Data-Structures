4c1ff8cf98689cc5bb57b033766ff0fe8c33b479
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-01-04 19:09:41 +0530

    Create Node.java

---
+++ Tree/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+  public String name;
+  public int Empno;
+  
+  public Node leftChild,rightChild;
+  
+  public void displayNode() {
+	  System.out.println("EmpNo \t" + Empno + "\t Name \t " + name);
+  }
+}
