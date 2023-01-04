65db12175cbe1bf2f742cd0468486a4921d8f5f2
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-01-04 19:08:07 +0530

    Create main.java

---
+++ Link/main.java
@@ -0,0 +1,17 @@
+public class Link {
+   public String name;
+   public double avg;
+   public Link next;
+   
+   public Link(String name, double avg) {
+	   this.name = name;
+	   this.avg = avg;
+	   this.next = null;
+	   
+   }
+   
+   public void displayDetails() {
+	   System.out.println("Name \t:" +name+ " || Avg :\t"+avg);
+	   System.out.println("---------------");
+   }
+}
